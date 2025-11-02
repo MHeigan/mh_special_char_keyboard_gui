6a04e7653218682879ff14ae6b3d3aae5525322d
Martin <35819373+MHeigan@users.noreply.github.com>
2025-11-02 15:29:31 +0200

    Delete _build_exe.bat

--- _build_exe.bat
+++
@@ -1,5 +0,0 @@
-pip install pyinstaller
-pyinstaller --noconfirm --onefile --windowed ^
-  --name mh_special_char_keyboard ^
-  --version-file file_version_info.txt ^
-  mh_special_char_keyboard_gui.py
